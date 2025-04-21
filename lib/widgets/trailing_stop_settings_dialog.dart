8447efe9f63bd1a9358e508318dae4206748954d
kambizniki <kambiz.niknafs@gmail.com>
2025-04-21 10:00:41 +0330

    Update trailing_stop_settings_dialog.dart
    Update trailing_stop_settings_dialog.dart

--- lib/widgets/trailing_stop_settings_dialog.dart
+++ lib/widgets/trailing_stop_settings_dialog.dart
@@ -1,44 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:kambiz_robot/models/trailing_stop_model.dart';
-import 'package:kambiz_robot/widgets/trailing_stop_settings_dialog.dart';
 
-class TrailingStopWidget extends StatelessWidget {
+class TrailingStopSettingsDialog extends StatefulWidget {
   final TrailingStopModel trailingStopModel;
-  final Function(TrailingStopModel) onUpdate;
 
-  TrailingStopWidget({
-    required this.trailingStopModel,
-    required this.onUpdate,
-  });
+  TrailingStopSettingsDialog({required this.trailingStopModel});
+
+  @override
+  _TrailingStopSettingsDialogState createState() =>
+      _TrailingStopSettingsDialogState();
+}
+
+class _TrailingStopSettingsDialogState
+    extends State<TrailingStopSettingsDialog> {
+  late double _stopLossPercentage;
+
+  @override
+  void initState() {
+    super.initState();
+    _stopLossPercentage = widget.trailingStopModel.stopLossPercentage;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () async {
-        final updatedModel = await showDialog<TrailingStopModel>(
-          context: context,
-          builder: (context) => TrailingStopSettingsDialog(
-            trailingStopModel: trailingStopModel,
+    return AlertDialog(
+      title: Text('Trailing Stop Settings'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text('Stop Loss Percentage: ${_stopLossPercentage.toStringAsFixed(1)}%'),
+          Slider(
+            value: _stopLossPercentage,
+            min: 0.1,
+            max: 20.0,
+            divisions: 200,
+            label: _stopLossPercentage.toStringAsFixed(1),
+            onChanged: (value) {
+              setState(() {
+                _stopLossPercentage = value;
+              });
+            },
           ),
-        );
-        if (updatedModel != null) {
-          onUpdate(updatedModel);
-        }
-      },
-      child: Container(
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Row(
-          children: [
-            Text('Trailing Stop'),
-            Spacer(),
-            Text('${trailingStopModel.stopLossPercentage}%'),
-          ],
-        ),
+        ],
       ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: Text('Cancel'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            final updatedModel = TrailingStopModel(
+              stopLossPercentage: _stopLossPercentage,
+            );
+            Navigator.of(context).pop(updatedModel);
+          },
+          child: Text('Save'),
+        ),
+      ],
     );
   }
 }
