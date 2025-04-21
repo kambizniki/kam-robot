8e3bf0ddadda6eff2f4582aac6d0533e1f3bf38b
kambizniki <kambiz.niknafs@gmail.com>
2025-04-21 09:56:02 +0330

    Update trailing_stop_settings_dialog.dart
    Update trailing_stop_settings_dialog.dart

--- lib/widgets/trailing_stop_settings_dialog.dart
+++ lib/widgets/trailing_stop_settings_dialog.dart
@@ -1,87 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:kambiz_robot/models/trailing_stop_model.dart';
+import 'package:kambiz_robot/widgets/trailing_stop_settings_dialog.dart';
 
-class TrailingStopSettingsDialog extends StatefulWidget {
-  final double initialActivationPrice;
-  final double initialCallbackRate;
-  final void Function(double activationPrice, double callbackRate) onSave;
+class TrailingStopWidget extends StatelessWidget {
+  final TrailingStopModel trailingStopModel;
+  final Function(TrailingStopModel) onUpdate;
 
-  const TrailingStopSettingsDialog({
-    super.key,
-    required this.initialActivationPrice,
-    required this.initialCallbackRate,
-    required this.onSave,
+  TrailingStopWidget({
+    required this.trailingStopModel,
+    required this.onUpdate,
   });
 
   @override
-  _TrailingStopSettingsDialogState createState() =>
-      _TrailingStopSettingsDialogState();
-}
-
-class _TrailingStopSettingsDialogState
-    extends State<TrailingStopSettingsDialog> {
-  late TextEditingController activationPriceController;
-  late TextEditingController callbackRateController;
-
-  @override
-  void initState() {
-    super.initState();
-    activationPriceController = TextEditingController(
-        text: widget.initialActivationPrice.toStringAsFixed(2));
-    callbackRateController = TextEditingController(
-        text: widget.initialCallbackRate.toStringAsFixed(2));
-  }
-
-  @override
-  void dispose() {
-    activationPriceController.dispose();
-    callbackRateController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Trailing Stop Settings'),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
-            controller: activationPriceController,
-            keyboardType:
-                const TextInputType.numberWithOptions(decimal: true),
-            decoration: const InputDecoration(
-              labelText: 'Activation Price (USDT)',
-            ),
+    return GestureDetector(
+      onTap: () async {
+        final updatedModel = await showDialog<TrailingStopModel>(
+          context: context,
+          builder: (context) => TrailingStopSettingsDialog(
+            trailingStopModel: trailingStopModel,
           ),
-          TextField(
-            controller: callbackRateController,
-            keyboardType:
-                const TextInputType.numberWithOptions(decimal: true),
-            decoration: const InputDecoration(
-              labelText: 'Callback Rate (%)',
-            ),
-          ),
-        ],
+        );
+        if (updatedModel != null) {
+          onUpdate(updatedModel);
+        }
+      },
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          children: [
+            Text('Trailing Stop'),
+            Spacer(),
+            Text('${trailingStopModel.stopLossPercentage}%'),
+          ],
+        ),
       ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: const Text('Cancel'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            final activationPrice =
-                double.tryParse(activationPriceController.text) ?? 0.0;
-            final callbackRate =
-                double.tryParse(callbackRateController.text) ?? 0.0;
-            widget.onSave(activationPrice, callbackRate);
-            Navigator.of(context).pop();
-          },
-          child: const Text('Save'),
-        ),
-      ],
     );
   }
 }
